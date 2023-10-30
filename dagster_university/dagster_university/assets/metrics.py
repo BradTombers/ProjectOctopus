9482a4dcc7402000450da3a35ffb3021b4729a2b
Brad Tombers <Brad.Tombers@gmail.com>
2023-10-30 08:42:16 -0500

    Update metrics.py

--- dagster_university/dagster_university/assets/metrics.py
+++ dagster_university/dagster_university/assets/metrics.py
@@ -58,51 +58,51 @@ def manhattan_map():
 
 @asset(deps=["taxi_trips"])
 def trips_by_week(database: DuckDBResource):
-    with database.get_connection() as conn:
-        current_date = datetime.strptime("2023-03-01", constants.DATE_FORMAT)
-        end_date = datetime.strptime("2023-04-01", constants.DATE_FORMAT)
+    current_date = datetime.strptime("2023-03-01", constants.DATE_FORMAT)
+    end_date = datetime.strptime("2023-04-01", constants.DATE_FORMAT)
 
-        result = pd.DataFrame()
+    result = pd.DataFrame()
 
-        while current_date < end_date:
-            current_date_str = current_date.strftime(constants.DATE_FORMAT)
-            query = f"""
-                SELECT
-                    vendor_id, total_amount, trip_distance, passenger_count
-                FROM trips
-                WERE DATE_TRUNC('week', pickup_datetime) = DATE_TRUNC('week', '{current_date_str}'::DATE)
-            """
+    while current_date < end_date:
+        current_date_str = current_date.strftime(constants.DATE_FORMAT)
+        query = f"""
+            SELECT
+                vendor_id, total_amount, trip_distance, passenger_count
+            FROM trips
+            WERE DATE_TRUNC('week', pickup_datetime) = DATE_TRUNC('week', '{current_date_str}'::DATE)
+        """
 
+        with database.get_connection() as conn:
             data_for_week = conn.execute(query).fetch_df()
 
-            aggregate = (
-                data_for_week.agg(
-                    {
-                        "vendor_id": "count",
-                        "total_amount": "sum",
-                        "trip_distance": "sum",
-                        "passenger_count": "sum",
-                    }
-                )
-                .rename({"vendor_id": "num_trips"})
-                .to_frame()
-                .T
-            )  # type: ignore
+        aggregate = (
+            data_for_week.agg(
+                {
+                    "vendor_id": "count",
+                    "total_amount": "sum",
+                    "trip_distance": "sum",
+                    "passenger_count": "sum",
+                }
+            )
+            .rename({"vendor_id": "num_trips"})
+            .to_frame()
+            .T
+        )  # type: ignore
 
-            aggregate["period"] = current_date
+        aggregate["period"] = current_date
 
-            result = pd.concat([result, aggregate])
+        result = pd.concat([result, aggregate])
 
-            current_date += timedelta(days=7)
+        current_date += timedelta(days=7)
 
-        # clean up the formatting of the dataframe
-        result["num_trips"] = result["num_trips"].astype(int)
-        result["passenger_count"] = result["passenger_count"].astype(int)
-        result["total_amount"] = result["total_amount"].round(2).astype(float)
-        result["trip_distance"] = result["trip_distance"].round(2).astype(float)
-        result = result[
-            ["period", "num_trips", "total_amount", "trip_distance", "passenger_count"]
-        ]
-        result = result.sort_values(by="period")
+    # clean up the formatting of the dataframe
+    result["num_trips"] = result["num_trips"].astype(int)
+    result["passenger_count"] = result["passenger_count"].astype(int)
+    result["total_amount"] = result["total_amount"].round(2).astype(float)
+    result["trip_distance"] = result["trip_distance"].round(2).astype(float)
+    result = result[
+        ["period", "num_trips", "total_amount", "trip_distance", "passenger_count"]
+    ]
+    result = result.sort_values(by="period")
 
-        result.to_csv(constants.TRIPS_BY_WEEK_FILE_PATH, index=False)
+    result.to_csv(constants.TRIPS_BY_WEEK_FILE_PATH, index=False)
